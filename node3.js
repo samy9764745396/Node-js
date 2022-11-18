7d687fa878d9d09ec0ccfeec19f37eb7062b1621
sayam <sampawar159@gmail.com>
2022-11-18 22:02:52 +0530

    files added

---
+++ node3.js
@@ -0,0 +1,29 @@
+
+const fs = require("fs");
+const https = require("http").createServer((request, response) => {
+  const myFileWriter = async (fileName, fileContent) => {
+    await new Promise((resolve) => {
+      fs.writeFile(fileName, fileContent, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log("File Created");
+      });
+    });
+  };
+  myFileWriter(
+    "index.html",
+    `<h1>Hello World</h1>
+<p>This is Sameer...</p>`
+  );
+  fs.readFile("./index.html", null, function (error, data) {
+    if (error) {
+      response.writeHead(404);
+      respone.write("Whoops! File not found!");
+    } else {
+      response.write(data);
+    }
+    response.end();
+  });
+});
+https.listen(5000, () => console.log("server running at 5000"));
